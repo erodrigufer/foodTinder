5153c6089d9cb980a7380c97c885f0d5faeaf30a
erodrigufer <erodrigufer@gmail.com>
2023-03-22 19:21:25 +0100

    Improved data model for application

--- cmd/foodTinder/main.go
+++ cmd/foodTinder/main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/erodrigufer/foodTinder/internal/api"
 )
 
 func main() {
 
-	// Create a logger for INFO messages, the prefix "INFO" and a tab will be
-	// displayed before each log message. The flags Ldate and Ltime provide the
-	// local date and time.
-	// infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	port := 8000
 
-	// Create an ERROR messages logger, additionally use the Lshortfile flag to
-	// display the file's name and line number for the error.
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app := api.NewApplication(port)
 
-	srv := api.NewServer(8000)
+	app.InfoLog.Printf("Starting server at port %d", port)
 
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	err := app.Srv.ListenAndServe()
+	app.ErrorLog.Fatal(err)
 
 }
---
+++ internal/api/model.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+// Application, object to interact with API from within main cmd file.
+type Application struct {
+	// Srv, *http.Server
+	Srv *http.Server
+	// ErrorLog, log server errors.
+	ErrorLog *log.Logger
+	// InfoLog, informative server logger.
+	InfoLog *log.Logger
+}
+
+// NewApplication, create a new Application struct that hosts the loggers and
+// http.Server
+func NewApplication(port int) *Application {
+	app := new(Application)
+	// Create a logger for INFO messages, the prefix "INFO" and a tab will be
+	// displayed before each log message. The flags Ldate and Ltime provide the
+	// local date and time.
+	app.InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create an ERROR messages logger, additionally use the Lshortfile flag to
+	// display the file's name and line number for the error.
+	app.ErrorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app.Srv = newServer(port, app.ErrorLog)
+
+	return app
+}
