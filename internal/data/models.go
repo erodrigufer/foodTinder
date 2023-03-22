337e19a27ec931d3fb6329e5026333b98f5a6fa3
erodrigufer <erodrigufer@gmail.com>
2023-03-22 23:02:06 +0100

    Fix bug, return error properly

--- internal/data/models.go
+++ internal/data/models.go
@@ -35,9 +35,10 @@ func (s SessionModel) Insert(session *Session) error {
 
 	args := []interface{}{session.ID}
 
-	result, err := s.DB.Exec(query, args...)
-	fmt.Println(result)
-
+	_, err := s.DB.Exec(query, args...)
+	if err != nil {
+		err = fmt.Errorf("error inserting session in db: %w", err)
+	}
 	return err
 }
 func (s SessionModel) Get(id int64) (*Session, error) {
